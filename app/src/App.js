1153729ff69196c4ee7bdf95d8ea2deae0d9b358
jackson.xu <a0975221909@gmail.com>
2023-11-11 19:49:00 +0800

    :sparkles: add new feature

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,12 @@
 import MuiAppBar from './components/MuiAppBar';
-
+import Dashboard from './pages/dashboard/index';
 
 function App() {
   return (
+    <div>
       <MuiAppBar />
+      <Dashboard />
+    </div>
   );
 }
 
---
+++ app/src/pages/dashboard/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Container from '@mui/material/Container';
+
+
+class Dashboard extends React.Component {
+  render() {
+    return (
+      <Container fixed>
+        <h3>Dashboard</h3>
+      </Container>
+    );
+  }
+}
+
+export default Dashboard;
